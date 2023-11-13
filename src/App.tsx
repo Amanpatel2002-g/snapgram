92b657cd3d90c3b5e3bed25ba17e50c75b85980e
Amanpatel2002-g <apofficial2002@gmail.com>
2023-11-13 21:24:16 +0530

    setup completed

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './globals.css'
+
+export const App = () => {
+    return (
+        <main>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </main>
+    )
+}
