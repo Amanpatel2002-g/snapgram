89c2910b48bb055785d893b6637c95b9cbcdee26
Amanpatel2002-g <apofficial2002@gmail.com>
2023-11-14 17:45:20 +0530

    shadCN setup completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import { Route, Routes } from 'react-router-dom'
 import './globals.css'
+import SignInForm from './_auth/forms/SignInForm'
+import { Home } from './_auth/_root/pages'
+import SignUpForm from './_auth/forms/SignUpForm'
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_auth/_root/RootLayout'
 
 export const App = () => {
     return (
-        <main>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
+        <main className='flex h- screen'>
+            <Routes>
+                {/* public Routes */}
+                <Route element={<AuthLayout />}>
+                    <Route path='/sign-in' element={<SignInForm />} />
+                    <Route path='/sign-up' element={<SignUpForm />} />
+                </Route>
+                {/* private Routes */}
+                <Route element={<RootLayout />}>
+                    <Route index element={<Home />} />
+                </Route>
+            </Routes>
         </main>
-    )
+    );
 }
+
---
+++ src/_auth/forms/SignUpForm.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+const SignUpForm = () => {
+  return (
+    <div>
+      <Button>
+        click here
+      </Button>
+
+    </div>
+  )
+}
+
+export default SignUpForm
